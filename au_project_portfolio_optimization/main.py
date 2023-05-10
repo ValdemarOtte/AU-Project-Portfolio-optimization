659fa1566f851d487f0d3113429100073987014c
Valdemar Emil Otte Petersen <36330221+ValdemarOtte@users.noreply.github.com>
2023-05-10 15:57:52 +0200

    subtask 3

--- au_project_portfolio_optimization/main.py
+++ au_project_portfolio_optimization/main.py
@@ -5,6 +5,7 @@ import pandas as pd
 from pandas.core.frame import DataFrame
 import pandas_datareader as pdr
 from scipy.stats import norm
+import scipy.optimize as scp_op
 
 
 # Matplotlib Style
@@ -77,7 +78,7 @@ def calculate_r(df_prices: DataFrame) -> ndarray:
     """
     # We use the function pct_change from Pandas. This give us the datafram where
     # df_{j, i} = (p_{j, i} - p_{j-1, i}) / p_{j, i}. This is not the right represents
-    # For returning the righ format, we skift the df ones up and delete the last row
+    # For returning the right format, we shift the df ones up and delete the last row
     df = df_prices.pct_change()
     df = df.shift(periods=-1)
     df.drop(df.tail(1).index, inplace=True)
@@ -109,7 +110,7 @@ def calculate_sigma(r: ndarray, mu: ndarray) -> ndarray:
     Returns
         sigma: Covariance matrix between the stocks
     """
-    sigma = (1 / r.shape[0]) * ((np.matmul((r - mu).transpose(), ((r - mu)))))
+    sigma = (1 / r.shape[0]) * ((r - mu).transpose() @ (r - mu))
     return sigma
 
 
@@ -125,12 +126,12 @@ def plot_pdf_norm(stocks: list[str], mu: ndarray, sigma: ndarray) -> None:
     # We use numpy's linspace to get a list of points, which our norm.pdf needs
     # No matter what, the the plot will show 95.3% of the distribution
     x = np.linspace(
-        min(mu) - 2 * max(np.diag(sigma)), max(mu) + 2 * max(np.diag(sigma))
+        min(mu) - 2 * max(np.diag(sigma)), max(mu) + 2 * max(np.diag(sigma)), 250
     )
     for i, stock in enumerate(stocks):
-        plt.plot(x, norm.pdf(x, mu[i], sigma[i][i]))
+        plt.plot(x, norm.pdf(x, mu[i], sigma[i][i]), label=stock)
 
-    plt.legend(stocks)
+    plt.legend()
     plt.title("Pdf of rate of return on stocks")
     plt.xlabel("Rate of returns")
     plt.show()
@@ -139,6 +140,73 @@ def plot_pdf_norm(stocks: list[str], mu: ndarray, sigma: ndarray) -> None:
 # ------------------------------
 #   Subtask 3)
 # ------------------------------
+def optimize(mu: ndarray, sigma: ndarray, gamma: ndarray) -> ndarray:
+    """
+    Parameters
+        mu: Array with the estimated means for the stocks
+        sigma: Covariance matrix between the stocks
+        gamma: Numpy array with values from a gamma distribution
+
+    Returns
+        solution: Array with optimale fractions of stocks
+    """
+    # We use put the object function inside the optimize function
+    # so we get a new function of w everytime
+    # We use the Scipy optimization extension and use the minimize fundtion
+    # The function used will be a new one everytime,
+    # so therefore we definde the function to be a Lambdafunction
+    # Lastly as per the constraints given we set the constraints to be equal, as per 'type': 'eq'
+    def obj_func(w):
+        return -(w.transpose() @ mu - gamma * w.transpose() @ sigma @ w)
+
+    n = len(mu)
+    optimize = scp_op.minimize(
+        obj_func,
+        np.ones(n) / n,
+        constraints={"fun": lambda w: np.sum(w) - 1, "type": "eq"},
+    )
+    solution = optimize.x
+    return solution
+
+
+def plot_gamma_pdf(solutions: list[ndarray], mu: ndarray, sigma: ndarray, gammas: ndarray) -> None:
+    """
+    Parameters
+        mu: Array with the estimated means for the stocks
+        sigma: Covariance matrix between the stocks
+        gamma: Numpy array with values from a gamma distribution
+        solutions: Array with optimale fractions of stocks
+    """
+    y = np.linspace(0, 2 * (solutions[0].transpose() @ mu), 250)
+
+    for solution in solutions:
+        x = norm.pdf(
+            y, solution.transpose() @ mu, solution.transpose() @ sigma @ solution
+        )
+        plt.plot(y, x)
+
+    plt.legend(gammas)
+    plt.title("Pdf of rate of return for each solution")
+    plt.xlabel("Rate of returns")
+    plt.show()
+
+
+def plot_w_scatter(solutions: list[ndarray], gammas: ndarray, stocks: list[str]) -> None:
+    """
+    Parameters
+        stocks: A list of the names of the stocks
+        gamma: Numpy array with values from a gamma distribution
+        solutions: Array with optimale fractions of stocks
+    """
+    for i, stock in enumerate(stocks):
+        fraction = [solutions[j][i] for j in range(len(solutions))]
+        plt.scatter(gammas, fraction, label=stock)
+
+    plt.legend()
+    plt.title("Scatter-plot of stock fractions for each solution")
+    plt.xlabel("Gammas (risk aversion)")
+    plt.ylabel("Fraction of stock")
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -150,6 +218,7 @@ if __name__ == "__main__":
     period = ("2012-01-01", "2021-01-01")
 
     stocks, df_prices = get_prices(stocks, step_size, period)
+
     plot_stocks(df_prices)
 
     # ------------------------------
@@ -158,8 +227,15 @@ if __name__ == "__main__":
     r = calculate_r(df_prices)
     mu = calculate_mu(r)
     sigma = calculate_sigma(r, mu)
+
     plot_pdf_norm(stocks, mu, sigma)
 
     # ------------------------------
     #   Subtask 3)
     # ------------------------------
+    gammas = (np.arange(10) / 5) + 1
+    solutions = [optimize(mu, sigma, gamma) for gamma in gammas]
+    print(type(solutions))
+
+    plot_gamma_pdf(solutions, mu, sigma, gammas)
+    plot_w_scatter(solutions, gammas, stocks)
